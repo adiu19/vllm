6f49a2b16b0b7fd83b9816c7070395290cc8e237
adiu19 <aditupadhyaya@gmail.com>
2026-05-11 00:03:31 -0700

    log

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """vLLM: a high-throughput and memory-efficient inference engine for LLMs"""
 
+print("*** running adiu19/vllm-fork ***")
+
 # The version.py should be independent library, and we always import the
 # version library first.  Such assumption is critical for some customization.
 from .version import __version__, __version_tuple__  # isort:skip
